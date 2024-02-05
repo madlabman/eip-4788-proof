996cb88ed7b7f4fe22e3c4fc1fd6e4da04306757
madlabman <10616301+madlabman@users.noreply.github.com>
2024-02-05 20:31:27 +0100

    feat: simplify withdrawal script

--- script/withdrawal.js
+++ script/withdrawal.js
@@ -1,96 +1,29 @@
-// NOTE: run with node --max-old-space-size=4096 withdrawal.js
-import fs from 'fs';
-
-import Assembler from 'stream-json/Assembler.js';
-import StreamChain from 'stream-chain';
-import StreamJson from 'stream-json';
-
-import { Tree, concatGindices } from '@chainsafe/persistent-merkle-tree';
+import { createProof, ProofType } from '@chainsafe/persistent-merkle-tree';
 import { ssz } from '@lodestar/types';
 
 import { createClient } from './client.js';
 import { toHex, verifyProof } from './utils.js';
 
-async function main(withdrawalIndex = 0) {
+const BeaconBlock = ssz.deneb.BeaconBlock;
+
+async function main(slot = 'finalized', withdrawalIndex = 0) {
     const client = await createClient();
+    let r;
 
-    const { parser } = StreamJson;
-    const { chain } = StreamChain;
-
-    // @type {ssz.deneb.BeaconState}
-    let state;
-
-    // Reading previously downloaded state response from disk in a stream fashion (slot 7424512)
-    // $ wget http://unstable.prater.beacon-api.nimbus.team/eth/v2/debug/beacon/states/7424512
-    // NOTE: Alternatively, fetch the state using client.debug.getState('7424512');
-    const pipeline = chain([fs.createReadStream('state.json'), parser()]);
-
-    // creating a plain object from the read stream
-    const asm = Assembler.connectTo(pipeline);
-    let r = await new Promise((resolve) => {
-        asm.on('done', function (r) {
-            resolve(r.current);
-        });
-    });
-
-    state = ssz.deneb.BeaconState.fromJson(r.data);
-    const slot = state.slot;
-
-    // requesting the corresponding beacon block to fetch withdrawals
+    // Requesting the corresponding beacon block to fetch withdrawals.
     r = await client.beacon.getBlockV2(slot);
     if (!r.ok) {
         throw r.error;
     }
 
-    // @type {ssz.deneb.BeaconBlock}
-    const block = r.response.data.message;
+    const blockView = BeaconBlock.toView(r.response.data.message);
+    const blockRoot = blockView.hashTreeRoot();
 
-    let tree,
-        gI,
-        p,
-        overallProof,
-        overallGi;
-
-    // block.root -> state_root
-    tree = new Tree(ssz.deneb.BeaconBlock.toView(block).node);
-    gI = ssz.deneb.BeaconBlock.getPropertyGindex('stateRoot');
-    p = tree.getSingleProof(gI);
-    const blockRoot = tree.root;
-
-    overallGi = gI;
-    overallProof = [...p];
-
-    // state_root -> withdrawals_root
-    tree = new Tree(ssz.deneb.BeaconState.toView(state).node); // consumes a lot of memory
-    gI = concatGindices([
-        ssz.deneb.BeaconState.getPropertyGindex(
-            'latestExecutionPayloadHeader'
-        ),
-        ssz.deneb.ExecutionPayloadHeader.getPropertyGindex('withdrawalsRoot'),
-    ]);
-    p = tree.getSingleProof(gI);
-
-    overallGi = concatGindices([overallGi, gI]);
-    // proofs are built from bottom up, so we need to prepend the proof
-    overallProof = [...p, ...overallProof];
-
-    // withdrawals_root -> withdrawal[idx].root
-    tree = new Tree(
-        ssz.capella.Withdrawals.toView(
-            block.body.executionPayload.withdrawals
-        ).node
-    );
-    gI = ssz.capella.Withdrawals.getPropertyGindex(withdrawalIndex);
-    p = tree.getSingleProof(gI);
-
-    overallGi = concatGindices([overallGi, gI]);
-    overallProof = [...p, ...overallProof];
-
-    const withdrawal = block.body.executionPayload.withdrawals[withdrawalIndex];
-    tree = new Tree(ssz.capella.Withdrawal.toView(withdrawal).node);
+    const nav = blockView.type.getPathInfo(['body', 'executionPayload', 'withdrawals', withdrawalIndex]);
+    const p = createProof(blockView.node, { type: ProofType.single, gindex: nav.gindex });
 
     // Sanity check: verify gIndex and proof match.
-    verifyProof(blockRoot, overallGi, overallProof, tree.root);
+    verifyProof(blockRoot, nav.gindex, p.witnesses, p.leaf);
 
     // Since EIP-4788 stores parentRoot, we have to find the descendant block of
     // the block from the state.
@@ -104,19 +37,16 @@ async function main(withdrawalIndex = 0) {
         throw new Error('No block to fetch timestamp from');
     }
 
-    // create output for the Verifier contract
+    // Create output for the Verifier contract.
     return {
         blockRoot: toHex(blockRoot),
-        proof: overallProof.map(toHex),
-        withdrawal: {
-            ...withdrawal,
-            address: toHex(withdrawal.address),
-        },
+        proof: p.witnesses.map(toHex),
+        withdrawal: nav.type.toJson(blockView.body.executionPayload.withdrawals.get(withdrawalIndex)),
         withdrawalIndex: withdrawalIndex,
         ts: client.slotToTS(nextBlock.header.message.slot),
+        gI: nav.gindex,
     };
-
 }
 
-main(9).then(console.log).catch(console.error);
-//   ^_ withdrawal index in withdrawals array
+main(7424512, 9).then(console.log).catch(console.error);
+//            ^_ withdrawal index in withdrawals array
